35198f25599a32dafbbd2f45f147153a0b48a9d8
Aryan Tajne <aryan.tajne@gmail.com>
2023-12-04 17:52:21 +0000

    first

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactPageScroller from 'react-page-scroller';
 
 function App() {
+  const handlePageChange = (number) => {
+    // do something when the page changes
+  };
+
   return (
-    <ReactPageScroller>
+    <ReactPageScroller
+      pageOnChange={handlePageChange}
+      customPageNumber={3} // the number of pages you have
+    >
       <div className="App">
         <header className="App-header">
           <p>
@@ -11,17 +18,17 @@ function App() {
           </p>
         </header>
       </div>
-      <div className="App">
+      <div className="App-intro">
         <header className="App-header">
           <p>
-            <b>Introduction</b>
+            Introduction
           </p>
         </header>
       </div>
-      <div className="App">
+      <div className="App-content">
         <header className="App-header">
           <p>
-            <b>What is AI?</b>
+            What is AI?
           </p>
         </header>
       </div>
