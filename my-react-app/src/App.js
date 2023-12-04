2a195aebf085b26903ea8f0e2d41cc6968856cbb
Aryan Tajne <aryan.tajne@gmail.com>
2023-12-04 17:44:27 +0000

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactPageScroller from 'react-page-scroller';
+
+function App() {
+  return (
+    <ReactPageScroller>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <b>Artificial Intelligence and the Future of Work: A Positive Outlook</b>
+          </p>
+        </header>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <b>Introduction</b>
+          </p>
+        </header>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <b>What is AI?</b>
+          </p>
+        </header>
+      </div>
+    </ReactPageScroller>
+  );
+}
+
+export default App;
